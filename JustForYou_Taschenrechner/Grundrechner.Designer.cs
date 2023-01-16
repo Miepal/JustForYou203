d9d423b56d08ffa94b4ab653a8b57c285b07926e
Füssel <fuesselni>
2023-01-16 12:18:56 +0100

    Button Funktionalität und beginn Rechenoperation

--- JustForYou_Taschenrechner/Grundrechner.Designer.cs
+++ JustForYou_Taschenrechner/Grundrechner.Designer.cs
@@ -52,9 +52,9 @@ namespace JustForYou_Taschenrechner
             this.b_comma = new System.Windows.Forms.Button();
             this.b_sign = new System.Windows.Forms.Button();
             this.btn_close = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.b_clear = new System.Windows.Forms.Button();
+            this.b_backspace = new System.Windows.Forms.Button();
+            this.b_settings = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tb_formula
@@ -73,6 +73,7 @@ namespace JustForYou_Taschenrechner
             this.b_plus.TabIndex = 1;
             this.b_plus.Text = "+";
             this.b_plus.UseVisualStyleBackColor = true;
+            this.b_plus.Click += new System.EventHandler(this.b_plus_Click);
             // 
             // b_minus
             // 
@@ -83,6 +84,7 @@ namespace JustForYou_Taschenrechner
             this.b_minus.TabIndex = 2;
             this.b_minus.Text = "-";
             this.b_minus.UseVisualStyleBackColor = true;
+            this.b_minus.Click += new System.EventHandler(this.b_minus_Click);
             // 
             // b_multiply
             // 
@@ -93,6 +95,7 @@ namespace JustForYou_Taschenrechner
             this.b_multiply.TabIndex = 3;
             this.b_multiply.Text = "x";
             this.b_multiply.UseVisualStyleBackColor = true;
+            this.b_multiply.Click += new System.EventHandler(this.b_multiply_Click);
             // 
             // b_divide
             // 
@@ -103,6 +106,7 @@ namespace JustForYou_Taschenrechner
             this.b_divide.TabIndex = 4;
             this.b_divide.Text = "/";
             this.b_divide.UseVisualStyleBackColor = true;
+            this.b_divide.Click += new System.EventHandler(this.b_divide_Click);
             // 
             // b_equal
             // 
@@ -113,6 +117,7 @@ namespace JustForYou_Taschenrechner
             this.b_equal.TabIndex = 5;
             this.b_equal.Text = "=";
             this.b_equal.UseVisualStyleBackColor = true;
+            this.b_equal.Click += new System.EventHandler(this.b_equal_Click);
             // 
             // label1
             // 
@@ -139,6 +144,7 @@ namespace JustForYou_Taschenrechner
             this.b_opening.TabIndex = 9;
             this.b_opening.Text = "(";
             this.b_opening.UseVisualStyleBackColor = true;
+            this.b_opening.Click += new System.EventHandler(this.b_opening_Click);
             // 
             // b_closing
             // 
@@ -149,6 +155,7 @@ namespace JustForYou_Taschenrechner
             this.b_closing.TabIndex = 10;
             this.b_closing.Text = ")";
             this.b_closing.UseVisualStyleBackColor = true;
+            this.b_closing.Click += new System.EventHandler(this.b_closing_Click);
             // 
             // b_3
             // 
@@ -159,6 +166,7 @@ namespace JustForYou_Taschenrechner
             this.b_3.TabIndex = 11;
             this.b_3.Text = "3";
             this.b_3.UseVisualStyleBackColor = true;
+            this.b_3.Click += new System.EventHandler(this.b_3_Click);
             // 
             // b_2
             // 
@@ -169,6 +177,7 @@ namespace JustForYou_Taschenrechner
             this.b_2.TabIndex = 12;
             this.b_2.Text = "2";
             this.b_2.UseVisualStyleBackColor = true;
+            this.b_2.Click += new System.EventHandler(this.b_2_Click);
             // 
             // b_1
             // 
@@ -179,6 +188,7 @@ namespace JustForYou_Taschenrechner
             this.b_1.TabIndex = 13;
             this.b_1.Text = "1";
             this.b_1.UseVisualStyleBackColor = true;
+            this.b_1.Click += new System.EventHandler(this.b_1_Click);
             // 
             // b_6
             // 
@@ -189,6 +199,7 @@ namespace JustForYou_Taschenrechner
             this.b_6.TabIndex = 14;
             this.b_6.Text = "6";
             this.b_6.UseVisualStyleBackColor = true;
+            this.b_6.Click += new System.EventHandler(this.b_6_Click);
             // 
             // b_5
             // 
@@ -199,6 +210,7 @@ namespace JustForYou_Taschenrechner
             this.b_5.TabIndex = 15;
             this.b_5.Text = "5";
             this.b_5.UseVisualStyleBackColor = true;
+            this.b_5.Click += new System.EventHandler(this.b_5_Click);
             // 
             // b_4
             // 
@@ -209,6 +221,7 @@ namespace JustForYou_Taschenrechner
             this.b_4.TabIndex = 16;
             this.b_4.Text = "4";
             this.b_4.UseVisualStyleBackColor = true;
+            this.b_4.Click += new System.EventHandler(this.b_4_Click);
             // 
             // b_9
             // 
@@ -219,6 +232,7 @@ namespace JustForYou_Taschenrechner
             this.b_9.TabIndex = 17;
             this.b_9.Text = "9";
             this.b_9.UseVisualStyleBackColor = true;
+            this.b_9.Click += new System.EventHandler(this.b_9_Click);
             // 
             // b_8
             // 
@@ -229,6 +243,7 @@ namespace JustForYou_Taschenrechner
             this.b_8.TabIndex = 18;
             this.b_8.Text = "8";
             this.b_8.UseVisualStyleBackColor = true;
+            this.b_8.Click += new System.EventHandler(this.b_8_Click);
             // 
             // b_7
             // 
@@ -239,6 +254,7 @@ namespace JustForYou_Taschenrechner
             this.b_7.TabIndex = 19;
             this.b_7.Text = "7";
             this.b_7.UseVisualStyleBackColor = true;
+            this.b_7.Click += new System.EventHandler(this.b_7_Click);
             // 
             // b_0
             // 
@@ -249,6 +265,7 @@ namespace JustForYou_Taschenrechner
             this.b_0.TabIndex = 20;
             this.b_0.Text = "0";
             this.b_0.UseVisualStyleBackColor = true;
+            this.b_0.Click += new System.EventHandler(this.b_0_Click);
             // 
             // b_comma
             // 
@@ -259,6 +276,7 @@ namespace JustForYou_Taschenrechner
             this.b_comma.TabIndex = 21;
             this.b_comma.Text = ",";
             this.b_comma.UseVisualStyleBackColor = true;
+            this.b_comma.Click += new System.EventHandler(this.b_comma_Click);
             // 
             // b_sign
             // 
@@ -269,6 +287,7 @@ namespace JustForYou_Taschenrechner
             this.b_sign.TabIndex = 22;
             this.b_sign.Text = "+/-";
             this.b_sign.UseVisualStyleBackColor = true;
+            this.b_sign.Click += new System.EventHandler(this.b_sign_Click);
             // 
             // btn_close
             // 
@@ -278,36 +297,40 @@ namespace JustForYou_Taschenrechner
             this.btn_close.TabIndex = 23;
             this.btn_close.Text = "Schließen";
             this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
             // 
-            // button3
+            // b_clear
             // 
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(193, 137);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(50, 49);
-            this.button3.TabIndex = 42;
-            this.button3.Text = "C";
-            this.button3.UseVisualStyleBackColor = true;
+            this.b_clear.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b_clear.Location = new System.Drawing.Point(193, 137);
+            this.b_clear.Name = "b_clear";
+            this.b_clear.Size = new System.Drawing.Size(50, 49);
+            this.b_clear.TabIndex = 42;
+            this.b_clear.Text = "C";
+            this.b_clear.UseVisualStyleBackColor = true;
+            this.b_clear.Click += new System.EventHandler(this.b_clear_Click);
             // 
-            // button4
+            // b_backspace
             // 
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(248, 137);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(50, 49);
-            this.button4.TabIndex = 41;
-            this.button4.Text = "⌫";
-            this.button4.UseVisualStyleBackColor = true;
+            this.b_backspace.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b_backspace.Location = new System.Drawing.Point(248, 137);
+            this.b_backspace.Name = "b_backspace";
+            this.b_backspace.Size = new System.Drawing.Size(50, 49);
+            this.b_backspace.TabIndex = 41;
+            this.b_backspace.Text = "⌫";
+            this.b_backspace.UseVisualStyleBackColor = true;
+            this.b_backspace.Click += new System.EventHandler(this.b_backspace_Click);
             // 
-            // button1
+            // b_settings
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(25, 381);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 50);
-            this.button1.TabIndex = 43;
-            this.button1.Text = "⚙";
-            this.button1.UseVisualStyleBackColor = true;
+            this.b_settings.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b_settings.Location = new System.Drawing.Point(25, 381);
+            this.b_settings.Name = "b_settings";
+            this.b_settings.Size = new System.Drawing.Size(50, 50);
+            this.b_settings.TabIndex = 43;
+            this.b_settings.Text = "⚙";
+            this.b_settings.UseVisualStyleBackColor = true;
+            this.b_settings.Click += new System.EventHandler(this.b_settings_Click);
             // 
             // Grundrechner
             // 
@@ -315,9 +338,9 @@ namespace JustForYou_Taschenrechner
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(313, 445);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.b_settings);
+            this.Controls.Add(this.b_clear);
+            this.Controls.Add(this.b_backspace);
             this.Controls.Add(this.btn_close);
             this.Controls.Add(this.b_sign);
             this.Controls.Add(this.b_comma);
@@ -373,8 +396,8 @@ namespace JustForYou_Taschenrechner
         private System.Windows.Forms.Button b_comma;
         private System.Windows.Forms.Button b_sign;
         private System.Windows.Forms.Button btn_close;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button b_clear;
+        private System.Windows.Forms.Button b_backspace;
+        private System.Windows.Forms.Button b_settings;
     }
 }
--- JustForYou_Taschenrechner/Grundrechner.cs
+++ JustForYou_Taschenrechner/Grundrechner.cs
@@ -5,14 +5,203 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace JustForYou_Taschenrechner
 {
     public partial class Grundrechner : Form
     {
+        private float result;
+
         public Grundrechner()
         {
             InitializeComponent();
+        }
+
+        public float getResult()
+        {
+            return result;
+        }
+
+        public void setResult(float data)
+        {
+            result = data;
+        }
+
+        private void b_divide_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '/';
+        }
+
+        private void b_multiply_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '*';
+        }
+
+        private void b_minus_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '-';
+        }
+
+        private void b_plus_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '+';
+        }
+
+        private void b_7_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '7';
+        }
+
+        private void b_8_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '8';
+        }
+
+        private void b_9_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text.Remove(0);
+        }
+
+        private void b_clear_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text = "";
+        }
+
+        private void b_backspace_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text.Remove(tb_formula.Text.Length - 1);
+        }
+
+        private void b_4_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '4';
+        }
+
+        private void b_5_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '5';
+        }
+
+        private void b_6_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '6';
+        }
+
+        private void b_opening_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '(';
+        }
+
+        private void b_closing_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += ')';
+        }
+
+        private void b_1_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '1';
+        }
+
+        private void b_2_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '2';
+        }
+
+        private void b_3_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '3';
+        }
+
+        private void b_sign_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '-';
+        }
+
+        private void b_0_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += '0';
+        }
+
+        private void b_comma_Click(object sender, EventArgs e)
+        {
+            tb_formula.Text += ',';
+        }
+
+        private void b_equal_Click(object sender, EventArgs e)
+        {
+            Regex regex = new Regex("\\(.*[^()].*\\)");
+            MatchCollection matches = regex.Matches(tb_formula.Text);
+            if(matches.Count != 0)
+            {
+                foreach (Match match in matches)
+                {
+
+                }
+            }
+
+        }
+
+        private void b_settings_Click(object sender, EventArgs e)
+        {
+            Einstellungen settings = new Einstellungen();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private string replace_MD(Match text)
+        {
+            Regex regex = new Regex("[0-9,/*]+");
+            string result = "";
+            MatchCollection matches = regex.Matches(text.Value);
+            if(matches.Count != 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Regex reg = new Regex("[0-9,]");
+                    MatchCollection numbers = reg.Matches(match.Value);
+                    List<double> values = new List<double>();
+                    foreach (Match number in numbers)
+                    {
+                        values.Add(Convert.ToDouble(number.Value));
+                    }
+                    Regex regexOperator = new Regex("[*/]");
+                    MatchCollection matchOperator = regexOperator.Matches(match.Value);
+                    foreach(Match o in matchOperator)
+                    {
+                        if(o.Value == "*")
+                        {
+                            values[0] = values[0] * values[1];
+                        }
+                        else
+                        {
+                            values[0] = values[0] / values[1];
+                        }
+                    }
+                    Regex calc = new Regex(match.Value);
+                    result = calc.Replace(text.Value, values[0].ToString());
+                }
+            }
+            return result;
+        }
+
+        private string replace_PM(Match text)
+        {
+            Regex regex = new Regex("[0-9,*/+-]*");
+            string result = "";
+            MatchCollection matches = regex.Matches(text.Value);
+            if(matches.Count != 0)
+            {
+                string[] replacer;
+                foreach(Match match in matches)
+                {
+
+                }
+            }
+            return result;
         }
     }
 }
